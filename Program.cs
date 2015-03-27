0de7f30116ee50fbe5e6d5664e1973c76256573e
Meeii <millesmsn@hotmail.se>
2015-03-27 19:32:53 +0100

    Part 1 - set up project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgBattleSimulator
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new RpgBattleSimulatorGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ RpgBattleSimulatorGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+
+namespace RpgBattleSimulator
+{
+    class RpgBattleSimulatorGame : Game
+    {
+        private GraphicsDeviceManager _deviceManager;
+        private SpriteBatch _spriteBatch;
+
+        public RpgBattleSimulatorGame()
+        {
+            _deviceManager = new GraphicsDeviceManager(this);
+            _deviceManager.PreferredBackBufferHeight = 480;
+            _deviceManager.PreferredBackBufferWidth = 800;
+            Content.RootDirectory = "Content"; 
+        }
+
+        protected override void Initialize()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            _spriteBatch.Begin();
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
